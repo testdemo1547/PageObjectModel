f7c287ee139ab99d0d3f8550f0c8bf8fa2542c70
testdemo1547 <81457414+testdemo1547@users.noreply.github.com>
2021-03-27 11:28:30 +0530

    Created push request
    update the code

--- Welcome.java
+++ Welcome.java
@@ -6,6 +6,7 @@ public static void main(String args[])
 System.out.println("Welcome to page object model");
   
   System.out.println("Added the second functions");
+    System.out.println("Created push request");
   
   
 }
